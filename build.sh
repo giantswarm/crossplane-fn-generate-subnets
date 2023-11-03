13f566d7e129b88cf93d60d2d2cfc696ab46ff9c
Martin Proffitt <mproffitt@choclab.net>
2023-11-03 19:53:03 +0100

    add rudimentary circleci

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 go build . && {
     rm package/*.xpkg
     go generate ./...
-    docker buildx build . -t docker.io/choclab/function-generate-subnets:v0.0.1
+    docker buildx build . -t docker.io/choclab/function-generate-subnets:v0.0.1-dev
     crossplane xpkg build -f package --embed-runtime-image=docker.io/choclab/function-generate-subnets:v0.0.1
     crossplane xpkg push -f package/$(ls package | grep function-generate) docker.io/choclab/function-generate-subnets:v0.0.1
 }
